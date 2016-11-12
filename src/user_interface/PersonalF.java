6ee1075dc869aeafc17bacb4e082cc99e2bcb312
Marilu Zambonino <andrea82z>
2016-11-12 12:41:59 -0500

    principal

--- src/user_interface/PersonalF.java
+++ src/user_interface/PersonalF.java
@@ -3,10 +3,25 @@ package user_interface;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTabbedPane;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Rectangle;
+
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import java.awt.Button;
+import java.awt.event.ActionListener;
+import java.awt.geom.Rectangle2D;
+import java.awt.event.ActionEvent;
+import java.awt.SystemColor;
 
 public class PersonalF extends JFrame {
 
@@ -35,6 +50,7 @@ public class PersonalF extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 942, 504);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(173, 216, 230));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
@@ -42,20 +58,147 @@ public class PersonalF extends JFrame {
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		tabbedPane.setBounds(47, 52, 836, 392);
 		contentPane.add(tabbedPane);
+		JPanel panel_0 = new JPanel();
+		panel_0.setBackground(new Color(220, 220, 220));
+		tabbedPane.addTab("Principal", null, panel_0, null);
+		panel_0.setLayout(null);
+		
+		JLabel lblIngenieriaDeSistemas = new JLabel("INGENIERIA DE SISTEMAS");
+		lblIngenieriaDeSistemas.setBounds(275, 35, 256, 29);
+		lblIngenieriaDeSistemas.setVerticalAlignment(SwingConstants.BOTTOM);
+		lblIngenieriaDeSistemas.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 18));
+		panel_0.add(lblIngenieriaDeSistemas);
+		
+		
+		
+		JLabel lblUniversidadPolitcnicaSalesiana = new JLabel("UNIVERSIDAD POLIT\u00C9CNICA SALESIANA");
+		lblUniversidadPolitcnicaSalesiana.setBounds(106, 11, 601, 35);
+		lblUniversidadPolitcnicaSalesiana.setForeground(Color.BLACK);
+		lblUniversidadPolitcnicaSalesiana.setFont(new Font("Dialog", Font.PLAIN, 27));
+		lblUniversidadPolitcnicaSalesiana.setBackground(Color.LIGHT_GRAY);
+		panel_0.add(lblUniversidadPolitcnicaSalesiana);
+		
+		JLabel lblInvestigacinDeOperaciones = new JLabel("INVESTIGACI\u00D3N DE OPERACIONES");
+		lblInvestigacinDeOperaciones.setVerticalAlignment(SwingConstants.BOTTOM);
+		lblInvestigacinDeOperaciones.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 15));
+		lblInvestigacinDeOperaciones.setBounds(275, 73, 256, 16);
+		panel_0.add(lblInvestigacinDeOperaciones);
+		
+		JLabel lblIntegrantesKarenAchig = new JLabel("Integrantes: ");
+		lblIntegrantesKarenAchig.setForeground(Color.DARK_GRAY);
+		lblIntegrantesKarenAchig.setVerticalAlignment(SwingConstants.BOTTOM);
+		lblIntegrantesKarenAchig.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 15));
+		lblIntegrantesKarenAchig.setBounds(33, 100, 126, 16);
+		panel_0.add(lblIntegrantesKarenAchig);
+		
+		JLabel lblKarenAchig = new JLabel("Karen Achig");
+		lblKarenAchig.setVerticalAlignment(SwingConstants.BOTTOM);
+		lblKarenAchig.setForeground(Color.BLACK);
+		lblKarenAchig.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 15));
+		lblKarenAchig.setBounds(125, 127, 126, 16);
+		panel_0.add(lblKarenAchig);
+		
+		JLabel lblJhonCastillo = new JLabel("Jhon Castillo");
+		lblJhonCastillo.setVerticalAlignment(SwingConstants.BOTTOM);
+		lblJhonCastillo.setForeground(Color.BLACK);
+		lblJhonCastillo.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 15));
+		lblJhonCastillo.setBounds(125, 150, 126, 16);
+		panel_0.add(lblJhonCastillo);
+		
+		JLabel lblJairoFlores = new JLabel("Jairo Flores");
+		lblJairoFlores.setVerticalAlignment(SwingConstants.BOTTOM);
+		lblJairoFlores.setForeground(Color.BLACK);
+		lblJairoFlores.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 15));
+		lblJairoFlores.setBounds(125, 177, 126, 16);
+		panel_0.add(lblJairoFlores);
+		
+		JLabel lblBryanTorres = new JLabel("Bryan Torres");
+		lblBryanTorres.setVerticalAlignment(SwingConstants.BOTTOM);
+		lblBryanTorres.setForeground(Color.BLACK);
+		lblBryanTorres.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 15));
+		lblBryanTorres.setBounds(125, 204, 126, 16);
+		panel_0.add(lblBryanTorres);
+		
+		JLabel lblMarilZambonino = new JLabel("Maril\u00FA Zambonino");
+		lblMarilZambonino.setVerticalAlignment(SwingConstants.BOTTOM);
+		lblMarilZambonino.setForeground(Color.BLACK);
+		lblMarilZambonino.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 15));
+		lblMarilZambonino.setBounds(125, 231, 126, 16);
+		panel_0.add(lblMarilZambonino);
+		
+		JLabel lblNivel = new JLabel("Nivel:");
+		lblNivel.setVerticalAlignment(SwingConstants.BOTTOM);
+		lblNivel.setForeground(Color.DARK_GRAY);
+		lblNivel.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 15));
+		lblNivel.setBounds(33, 272, 126, 16);
+		panel_0.add(lblNivel);
+		
+		JLabel lblno = new JLabel("9no");
+		lblno.setVerticalAlignment(SwingConstants.BOTTOM);
+		lblno.setForeground(Color.BLACK);
+		lblno.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 15));
+		lblno.setBounds(79, 273, 126, 16);
+		panel_0.add(lblno);
+		
+		JLabel lblIngWalterGaibor = new JLabel("Ing. Walter Gaibor");
+		lblIngWalterGaibor.setVerticalAlignment(SwingConstants.BOTTOM);
+		lblIngWalterGaibor.setForeground(Color.BLACK);
+		lblIngWalterGaibor.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 15));
+		lblIngWalterGaibor.setBounds(33, 308, 126, 16);
+		panel_0.add(lblIngWalterGaibor);
+		
+		JButton btnPDF = new JButton("");
+		btnPDF.setIcon(new ImageIcon("Z:\\ECLIPSE\\IOperacionesI\\imagenes\\logo.png")); 
+			btnPDF.setBounds(21, 11, 75, 78);
+		panel_0.add(btnPDF);
+		
+		JButton button = new JButton("");
+		button.setIcon(new ImageIcon("Z:\\ECLIPSE\\IOperacionesI\\imagenes\\principal.jpg"));
+		button.setBounds(363, 124, 240, 229);
+		panel_0.add(button);
+		
+		JLabel lblAnalytic = new JLabel("ANALYTIC");
+		lblAnalytic.setForeground(SystemColor.activeCaption);
+		lblAnalytic.setFont(new Font("Dialog", Font.PLAIN, 27));
+		lblAnalytic.setBackground(Color.LIGHT_GRAY);
+		lblAnalytic.setBounds(613, 134, 132, 35);
+		panel_0.add(lblAnalytic);
+		
+		JLabel lblHierarchy = new JLabel("HIERARCHY");
+		lblHierarchy.setForeground(new Color(255, 160, 122));
+		lblHierarchy.setFont(new Font("Dialog", Font.PLAIN, 27));
+		lblHierarchy.setBackground(Color.LIGHT_GRAY);
+		lblHierarchy.setBounds(623, 177, 188, 43);
+		panel_0.add(lblHierarchy);
+		
+		JLabel lblProcess = new JLabel("PROCESS");
+		lblProcess.setForeground(new Color(144, 238, 144));
+		lblProcess.setFont(new Font("Dialog", Font.PLAIN, 27));
+		lblProcess.setBackground(Color.LIGHT_GRAY);
+		lblProcess.setBounds(651, 231, 170, 43);
+		panel_0.add(lblProcess);
+		
 		
 		JPanel panel = new JPanel();
+		panel.setBackground(new Color(220, 220, 220));
 		tabbedPane.addTab("Añadir Variable", null, panel, null);
 		
 		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(new Color(220, 220, 220));
 		tabbedPane.addTab("Añadir Criterios", null, panel_1, null);
 		
 		JPanel panel_2 = new JPanel();
+		panel_2.setBackground(new Color(220, 220, 220));
 		tabbedPane.addTab("Tabla C", null, panel_2, null);
 		
 		JPanel panel_3 = new JPanel();
+		panel_3.setBackground(new Color(220, 220, 220));
 		tabbedPane.addTab("Tabla Llena", null, panel_3, null);
 		
 		JPanel panel_4 = new JPanel();
+		panel_4.setBackground(new Color(220, 220, 220));
 		tabbedPane.addTab("Matriz Cuadrada", null, panel_4, null);
+		
+	
 	}
 }
