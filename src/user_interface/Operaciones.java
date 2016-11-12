04a1fc65a4ddcd4de0fc36f2266efd9f91180fff
jairdean <jairdean@hotmail.com>
2016-11-12 17:25:58 -0500

    multiplicacion

--- src/user_interface/Operaciones.java
+++ src/user_interface/Operaciones.java
@@ -1,11 +1,14 @@
 package user_interface;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
 public class Operaciones {
 	double[][] multiplicacion= null;
 	double[][] A={
-			{1, 	0.5,	 3},
-			{2, 	1, 		4},
-			{0.33, 0.25, 	1}
+			{1.0, 	0.5,	 3.0},
+			{2.0, 	1.0, 		4.0},
+			{0.33, 0.25, 	1.0}
 			};
 	
 	int FILAS, COLUMNAS;
@@ -26,19 +29,24 @@ public class Operaciones {
 		
 		
 		multiplicacion = new double[fil_m1][col_m2];
-	
+
 		for (int x=0; x < multiplicacion.length; x++) {
-			  for (int i=0; i < multiplicacion[x].length; i++) {
-			    for (int j=0; j<col_m1; j++) {
-			      multiplicacion [x][j] += A[x][j]*A[j][i]; 
-			    }
-			  }
-			}
+			  for (int y=0; y < multiplicacion[x].length; y++){
+			    for (int z=0; z<col_m1; z++) {
+			    	if(x!=y){
+			    		multiplicacion [x][y] += A[x][z]*A[z][y];	
+			    	}else{
+			    		multiplicacion [x][y] += Math.round(A[x][z]*A[z][y]);
+			    	}
+			    }//z
+			  }//y
+			}//x
 		
-		int p,q;
-		for(p=0;p<multiplicacion.length;p++){
-			for(q=0;q<multiplicacion[p].length;q++){
-				System.out.print(multiplicacion[p][q]+"\t");
+		
+		int i,j;
+		for(i=0;i<multiplicacion.length;i++){
+			for(j=0;j<multiplicacion[i].length;j++){
+				System.out.print(multiplicacion[i][j]+"\t");
 			}
 			System.out.println();
 		}
